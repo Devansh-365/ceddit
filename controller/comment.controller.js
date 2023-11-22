6b244a78b768ede2d47e10651aa671798ecd87fd
mrcoder57 <tiwariji2300@gmail.com>
2023-11-22 19:31:24 +0530

    created api for comment and reply

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -1,87 +1,82 @@
-const { Comment, Reply } = require("../model/comment.model");
+const  Comment  = require("../model/comment.model");
 const Post = require("../model/post.model");
 
-const writeComment = async (req, res) => {
-  const postId = req.params.postId;
-  const { comment } = req.body;
+
+const writeCommentForPost = async (req, res) => {
   try {
+    // Extract necessary data from the request
+    const { content } = req.body;
+    const commentedBy=req.user.userId
+    const postId = req.params.postId;
+
+    // Find the post by ID
     const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+    if (!post.comments) {
+      post.comments = [];
+    }
+    // Create a new comment
+    const newComment = new Comment({
+      commentedBy,
+      content,
+    });
+
+    // Save the new comment to the database
+    await newComment.save();
+
+    // Add the comment to the post
+    post.comments.push(newComment);
+    post.commentCount += 1;
+
+    // Save the updated post
+    await post.save();
+
+    res.status(201).json({ message: 'Comment added successfully', comment: newComment });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error', error });
+  }
+};
+const addReply = async (req, res) => {
+  try {
+    const { content } = req.body;
+    const commentedBy=req.user.userId
+    const postId = req.params.postId;
+    const commentId=req.params.id;
+    
+    const comment = new Comment({
+      commentedBy: commentedBy,
+      content: content,
+      parents:commentId
+    });
+
+    await comment.save();
+
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
+
     if (!post.comments) {
       post.comments = [];
     }
 
-    const newComment = new Comment({
-      commentedBy: req.user.userId,
-      content: comment,
-    });
-    await newComment.save();
-
-    post.comments.push(newComment);
-    post.commentCount += 1;
+    post.comments.push(comment._id);
 
     await post.save();
 
-    res.json({
-      message: "Comment added successfully",
-      commentId: newComment._id,
-      comment: newComment,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error", err });
-  }
-};
-const addReply = async (req, res) => {
-  const commentId = req.params.id;
-  const { reply } = req.body;
-  try {
-    const comment = await Comment.findById(commentId);
-    if (!comment) {
-      return res.status(404).json({ message: "Comment not found" });
-    }
-    const newReply = new Reply({
-      repliedBy: req.user.userId,
-      content: reply,
-    });
-    await newReply.save();
-    const repliesLengthBefore = comment.replies.length;
-
-    comment.replies.push(newReply);
-
-    await comment.save();
-
-    const repliesLengthAfter = comment.replies.length;
-
-    const replyAdded = repliesLengthAfter > repliesLengthBefore;
-
-    res.json({
-      message: "Reply added successfully",
-      reply: newReply,
-      replyAdded,
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error", error });
-  }
-};
-const getCommentWithReplies = async (req, res) => {
-  const commentId = req.params.id;
-
-  try {
-    const comment = await Comment.findById(commentId).populate("replies");
-
-    if (!comment) {
-      return res.status(404).json({ message: "Comment not found" });
-    }
-
-    res.json({ message: "Comment retrieved successfully", comment });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error", error });
+    
+    res.status(201).json({ message: 'reply added successfully', comment: comment });
+   
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message:" could not save comment", error: err.message });
   }
 };
 module.exports = {
-  writeComment,
-  addReply,
-  getCommentWithReplies,
-};
+  
+  writeCommentForPost
+,addReply};
--- routes/comment.route.js
+++ routes/comment.route.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { verifyToken } = require("../middleware/auth");
 const comment = require("../controller/comment.controller");
 
-router.post('/:postId',verifyToken,comment.writeComment)
-router.post('/reply/:id',verifyToken,comment.addReply)
-router.get('/:id',comment.getCommentWithReplies)
+router.post('/:postId',verifyToken,comment.writeCommentForPost)
+router.post('/:postId/reply/:id',verifyToken,comment.addReply)
+// router.get('/:id',comment.getCommentWithReplies)
 
 module.exports = router;

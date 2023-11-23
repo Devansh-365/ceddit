267e23295a85db922a07e3e805e29b557c2ca63c
mrcoder57 <tiwariji2300@gmail.com>
2023-11-23 22:20:47 +0530

    Created up & downVote comment

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -141,10 +141,73 @@ const deleteCommentForPost = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error", error });
   }
 };
+const upvoteComment = async (req, res) => {
+  const commentId = req.params.commentId;
+  const userId = req.user.userId;
 
+  try {
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const hasUpvoted = comment.upvotedBy.includes(userId);
+
+    if (hasUpvoted) {
+      
+      comment.upvotedBy = comment.upvotedBy.filter((id) => id.toString() !== userId);
+      await comment.save();
+      return res.status(200).json({ message: "Successfully removed upvote from the comment" });
+    }
+
+    comment.upvotedBy.push(userId);
+    await comment.save();
+
+    return res.status(200).json({ message: "Successfully upvoted the comment" });
+  } catch (error) {
+    console.error(`Error upvoting comment: ${error.message}`);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const downvoteComment = async (req, res) => {
+  const commentId = req.params.commentId;
+  const userId = req.user.userId;
+
+  try {
+    const comment = await Comment.findById(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ error: "comment not found" });
+    }
+
+    const hasUpvoted = comment.upvotedBy.includes(userId);
+    const hasDownvoted = comment.downvotedBy.includes(userId);
+
+    if (hasUpvoted) {
+      comment.upvotedBy = comment.upvotedBy.filter((id) => id.toString() !== userId);
+    }
+
+    if (hasDownvoted) {
+      comment.downvotedBy = comment.downvotedBy.filter((id) => id.toString() !== userId);
+    } else {
+      comment.downvotedBy.push(userId);
+    }
+
+    await comment.save();
+
+    return res.status(200).json({ message: "Successfully downvoted the comment" });
+  } catch (error) {
+    console.error(`Error downvoting comment: ${error.message}`);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+};
 module.exports = {
   writeCommentForPost,
   addReply,
   updateCommentForPost,
   deleteCommentForPost,
+  upvoteComment,
+  downvoteComment
 };
--- routes/comment.route.js
+++ routes/comment.route.js
@@ -4,6 +4,8 @@ const { verifyToken } = require("../middleware/auth");
 const comment = require("../controller/comment.controller");
 
 router.post('/:postId',verifyToken,comment.writeCommentForPost)
+router.post('/:commentId/upvote',verifyToken,comment.upvoteComment)
+router.post('/:commentId/downvote',verifyToken,comment.downvoteComment)
 router.post('/:postId/reply/:id',verifyToken,comment.addReply)
 router.put('/:postId/:id',verifyToken,comment.updateCommentForPost)
 router.delete('/:postId/:id',verifyToken,comment.deleteCommentForPost)

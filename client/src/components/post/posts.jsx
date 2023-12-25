8e3d16d6acb4abfa6ddd1f2b14a756be268a3c42
Devansh-365 <devanshtiwari365@gmail.com>
2023-12-25 21:45:34 +0530

    feat(client): add infinte scroll posts

--- client/src/components/post/posts.jsx
+++ client/src/components/post/posts.jsx
@@ -1,20 +1,60 @@
 import React, { useEffect, useState } from "react";
-import { PostLoading } from "./post-loading";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Stack } from "@chakra-ui/react";
+import { PostLoading } from "./post-loading";
 import PostItem from "./post-item";
 import { getPosts } from "../../api/posts";
+import { BASE_URL } from "../../config";
 
 export const Posts = ({ posts }) => {
+  const [items, setItems] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [index, setIndex] = useState(2);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}api/posts/1`);
+        const data = await response.json();
+        setItems(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const fetchMoreData = async () => {
+    await fetch(`${BASE_URL}api/posts/${index}`)
+      .then(async (res) => {
+        const data = await res.json();
+        setItems((prevItems) => [...prevItems, ...data]);
+
+        data.length > 0 ? setHasMore(true) : setHasMore(false);
+      })
+      .catch((err) => console.log(err));
+
+    setIndex((prevIndex) => prevIndex + 1);
+  };
+
   return (
     <>
-      {posts.length === 0 ? (
+      {items.length === 0 ? (
         <PostLoading />
       ) : (
-        <Stack>
-          {posts.map((item, i) => (
-            <PostItem post={item} key={i} />
-          ))}
-        </Stack>
+        <InfiniteScroll
+          dataLength={items.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<PostLoading />}
+        >
+          <Stack>
+            {items.map((item, i) => (
+              <PostItem post={item} key={i} />
+            ))}
+          </Stack>
+        </InfiniteScroll>
       )}
     </>
   );
